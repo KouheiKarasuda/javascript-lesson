eea56605b1d13f6835ffe499f0872d289c2093d1
unknown <DUMMY>
2026-07-01 23:41:10 +0900

    first commit

---
+++ practice/app.js
@@ -0,0 +1,15 @@
+console.log('Hello World!');
+console.log('1つ目の値', '2つ目の値');
+
+let text = 'JavaScriptの練習';
+console.log(text);
+text = 'JavaScriptをマスターした';
+console.log(text);
+
+let name = 'ジョニー';
+let greet = '私は' + name + '！';
+console.log(greet);
+
+let numTen = 10;
+let strTen = '10'; // 追記
+console.log(numTen, strTen); // 値を追加
